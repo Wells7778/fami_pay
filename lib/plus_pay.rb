551ef221861155f52ebba324aae55f5df87fb73b
Wells <chawkeye@gmail.com>
2021-11-02 12:35:57 +0800

    init gem

---
+++ lib/plus_pay.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "plus_pay/version"
+
+module PlusPay
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/plus_pay/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module PlusPay
+  VERSION = "0.1.0"
+end
