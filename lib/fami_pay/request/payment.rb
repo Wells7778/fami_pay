e0eba81c25ab448736dbc34230aa5ee5d3b7e318
AndrewLiao <andrew@weibyapps.com>
2023-11-16 15:13:23 +0800

    add branch name and id in payment request

--- lib/fami_pay/request/payment.rb
+++ lib/fami_pay/request/payment.rb
@@ -1,57 +1,41 @@
 # frozen_string_literal: true
-require_relative "base"
+
+require_relative 'base'
 
 module FamiPay
   module Request
     class Payment < Base
+      attr_writer :timeout, :buyer_id, :trade_number, :amount, :store_name
 
-      def timeout= value_time_out
-        @timeout = value_time_out
-      end
-
-      def buyer_id= value_buyer_id
-        @buyer_id = value_buyer_id
-      end
-
-      def trade_number= value_trade_number
-        @trade_number = value_trade_number
-      end
-
-      def amount= value_amount
-        @amount = value_amount
-      end
-
-      def trade_time= value_trade_time
+      def trade_time=(value_trade_time)
         @trade_time = value_trade_time if value_trade_time.is_a? String
         @trade_time ||= value_trade_time.strftime('%Y%m%d%H%M%S')
-      end
-
-      def store_name= value_store_name
-        @store_name = value_store_name
       end
 
       private
 
       def to_hash
-        super.merge({
-          Timeout: @timeout || 20,
-          BuyerID: @buyer_id,
-          OrderNo: @trade_number,
-          OrderCurrency: "TWD",
-          OrderAmount: @amount,
-          OrderDT: @trade_time,
-          OrderTitle: @store_name,
-        })
+        hash = super.merge({
+                             Timeout: @timeout || 20,
+                             BuyerID: @buyer_id,
+                             OrderNo: @trade_number,
+                             OrderCurrency: 'TWD',
+                             OrderAmount: @amount,
+                             OrderDT: @trade_time,
+                             OrderTitle: @store_name
+                           })
+        hash[:BranchID] = branch_id if branch_id
+        hash[:BranchName] = branch_name if branch_name
+        hash
       end
 
       def request_type
-        "tradeapi"
+        'tradeapi'
       end
 
       def request_action
-        "payment"
+        'payment'
       end
-
     end
   end
 end
